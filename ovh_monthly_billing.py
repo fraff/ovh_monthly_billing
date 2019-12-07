d3003464d67baabed42a3173525f55f27799f876
Francois Lallart <fraff@free.fr>
2019-12-07 20:22:13 +0100

    some minor upates

--- ovh_monthly_billing.py
+++ ovh_monthly_billing.py
@@ -1,31 +1,24 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# TODO
-# . use *_id and *_name for project and instance
-# . get *_id from *_name
+# Copyright: Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-# ovh_monthly_billing, an Ansible module for managing OVH monthly billing
-# Copyright (C) 2019, Francois Lallart <fraff@free.fr>
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software Foundation,
-# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+ANSIBLE_METADATA = {
+    "metadata_version": "1.1",
+    "status": ["preview"],
+    "supported_by": "community",
+}
 
 DOCUMENTATION = '''
 ---
 module: ovh_monthly_billing
-author: Francois (fraff) Lallart, based on Carlos Izquierdo's ovh_dns
+author: Francois Lallart (@fraff)
+version_added: "2.10"
 short_description: Manage OVH monthly billing
 description:
     - Enable monthly billing on OVH cloud intances
@@ -33,14 +26,17 @@ requirements: [ "ovh" ]
 options:
     project_id:
         required: true
+        type: str
         description:
             - ID of the project, get it with https://api.ovh.com/console/#/cloud/project#GET
     instance_id:
         required: true
+        type: str
         description:
             - ID of the instance, get it with https://api.ovh.com/console/#/cloud/project/%7BserviceName%7D/instance#GET
     state:
         required: false
+        type: str
         default: present
         choices: ['present', 'absent']
         description:
@@ -49,7 +45,23 @@ options:
 
 EXAMPLES = '''
 # basic
-- ovh_monthly_billing: project_id=0c727a20aa144485b70c44dee9123b46 instance_id=8fa89ad2-8f08-4220-9fa4-9695ea23e948
+  - ovh_monthly_billing: project_id=0c727a20aa144485b70c44dee9123b46 instance_id=8fa89ad2-8f08-4220-9fa4-9695ea23e948
+
+# more complex
+  - os_server_facts:
+      cloud: myProjectName
+      region_name: myRegionName
+      server: myServerName
+    # force run even in check_mode
+    check_mode: no
+
+  - name: ovh_monthly_billing
+    ovh_monthly_billing:
+      project_id: "{{ openstack_servers.0.tenant_id }}"
+      instance_id: "{{ openstack_servers.0.id }}"
+'''
+
+RETURN = '''
 '''
 
 import os
@@ -59,19 +71,21 @@ try:
     import ovh
     import ovh.exceptions
     from ovh.exceptions import APIError
+    HAS_OVH = True
 except ImportError:
-    print "failed=True msg='ovh required for this module'"
-    sys.exit(1)
+    HAS_OVH = False
+
+from ansible.module_utils.basic import AnsibleModule
 
 
 def main():
     module = AnsibleModule(
-        argument_spec = dict(
-            project_id = dict(required=True),
-            instance_id = dict(required=True),
-            state = dict(default='present', choices=['present', 'absent']),
+        argument_spec=dict(
+            project_id=dict(required=True),
+            instance_id=dict(required=True),
+            state=dict(default='present', choices=['present', 'absent']),
         ),
-        supports_check_mode = True
+        supports_check_mode=True
     )
 
     # Get parameters
@@ -80,48 +94,41 @@ def main():
     state = module.params.get('state')
     project = ""
     instance = ""
-#    infos = {}
+    result = ""
 
     # Connect to OVH API
     client = ovh.Client()
 
-#    # get list of projects
-#    projects = client.get('/cloud/project')
-#    for p in projects:
-#        infos[p] = client.get('/cloud/project/{}'.format(p))
-#
-#    module.exit_json(msg=infos)
+    if not HAS_OVH:
+        module.fail_json(msg='python-ovh is required to run this module, see https://github.com/ovh/python-ovh')
 
     if state == "absent":
         module.fail_json(msg="OVH API does not allow to remove monthly billing")
 
     # Check that the instance exists
     try:
-        project = client.get('/cloud/project/{}'.format(project_id))
+        project = client.get('/cloud/project/{0}'.format(project_id))
     except ovh.exceptions.ResourceNotFoundError:
-        module.fail_json(msg='project {} does not exist'.format(project_id))
+        module.fail_json(msg='project {0} does not exist'.format(project_id))
 
     # Check that the instance exists
     try:
-        instance = client.get('/cloud/project/{}/instance/{}'.format(project_id, instance_id))
+        instance = client.get('/cloud/project/{0}/instance/{1}'.format(project_id, instance_id))
     except ovh.exceptions.ResourceNotFoundError:
-        module.fail_json(msg='instance {} does not exist in project {}'.format(instance_id, project_id))
-        # module.fail_json(msg='instance {} does not exist in project {}'.format(instance_id, project['description']))
+        module.fail_json(msg='instance {0} does not exist in project {1}'.format(instance_id, project_id))
 
-#    module.warn("this is a scary warning.")
-#    module.log(msg="this is a log() test sent to syslog")
-
+    # Is monthlyBilling already enabled or pending ?
     if instance['monthlyBilling'] is not None:
         if instance['monthlyBilling']['status'] in ['ok', 'activationPending']:
-            module.exit_json(changed=False, monthlyBilling=instance['monthlyBilling'])
+            module.exit_json(changed=False, result=instance['monthlyBilling'])
 
     if module.check_mode:
         module.exit_json(changed=True, msg="Dry Run!")
 
     else:
         try:
-            client.post('/cloud/project/{}/instance/{}/activeMonthlyBilling'.format(project_id, instance_id))
-            module.exit_json(changed=True)
+            result = client.post('/cloud/project/{0}/instance/{1}/activeMonthlyBilling'.format(project_id, instance_id))
+            module.exit_json(changed=True, result=result['monthlyBilling'])
         except APIError as apiError:
             module.fail_json(changed=False, msg="Failed to call OVH API: {0}".format(apiError))
 
@@ -129,7 +136,5 @@ def main():
     module.fail_json(msg='Internal ovh_monthly_billing module error')
 
 
-# import module snippets
-from ansible.module_utils.basic import *
-
-main()
+if __name__ == "__main__":
+    main()
